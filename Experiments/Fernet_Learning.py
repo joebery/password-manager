eca43fb552d54ea36138f9047b8a0b9673ca29f4
joebery <josephbery@gmail.com>
2026-04-21 19:53:36 +0100

    Playing around with Fernet a little bit

--- Experiments/Fernet_Learning.py
+++ Experiments/Fernet_Learning.py
@@ -8,24 +8,32 @@ Created on Tue Apr 21 18:38:50 2026
 from cryptography.fernet import Fernet
 
 def cyrpt(password):
-    key = Fernet.generate_key() # Generating a brand new key 
-    # This needs to be stored in a file, we will use salt.bin 
-    # Key = the secret 
-    #####THIS WILL BE STORED IN THE SALT.BIN####
     
     
+    
+    
+    try: 
+        with open("Fernet.bin" , 'rb') as p:
+            key = p.read()
+            print('d')
+    except FileNotFoundError:
+        print('o')
+        with open("Fernet.bin", "wb") as f:
+            key = Fernet.generate_key() # Generating a brand new key 
+            # Key = the secret 
+            f.write(key) # Adds to the file 
+
     cipher = Fernet(key) # This creates a Fernet object using the key 
     # Fernet the lock/unlock tool built from the secret 
     # Used to encrypt/decrypt 
     
-    
-    
-    
+    # print(type(key))
+
     encrypted = cipher.encrypt(password) # Encrypting the password 
-    print(encrypted)
+    print(f"This is the encrypted password: {encrypted}")
     
     decrypted = cipher.decrypt(encrypted) # Decrypting
-    print(decrypted) #####THIS IS WHAT WE WANT TO STORE IN THE JSON FILE#####
+    print(f"This is the decrypted password: {decrypted}") #####THIS IS WHAT WE WANT TO STORE IN THE JSON FILE#####
     return decrypted
     
     
